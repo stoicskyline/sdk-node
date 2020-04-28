5864eabf7c210199f7b5c9f0f90d5590c7fd8cc5
jorge <sylvanknave@gmail.com>
2020-04-27 19:59:25 -0500

    fix types in useFetch.ts, but not tested

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,34 +1,34 @@
-import { useEffect, useReducer, useRef } from 'react'
+import { Reducer, useEffect, useReducer, useRef } from 'react'
 
 // TODO test this, this is not tested
 // https://dev.to/silvestricodes/asynchronous-flows-with-react-hooks-1g0m comment https://dev.to/kevtiq/comment/f4d2
 
-const useFetch = (fetchPromise: Promise<any>, reducer, initialState, deps?: any[]) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const isMounted = useRef(false);
+export const useFetch = (fetchPromise: Promise<any>, reducer: Reducer<any, any>, initialState: any, deps?: any[]) => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const isMounted = useRef(false)
 
   useEffect(() => {
-    isMounted.current = true;
-    return () => { isMounted.current = false; }
-  }, []);
+    isMounted.current = true
+    return () => {
+      isMounted.current = false
+    }
+  }, [])
 
   useEffect(() => {
-    fetch();
-  }, deps); 
+    fetch()
+  }, deps)
 
-  async function fetch (vars) => {
-    if (!isMounted.current) return; 
-    dispatch({ type: 'INIT' });
+  async function fetch() {
+    if (!isMounted.current) return
+    dispatch({ type: 'INIT' })
 
     try {
-      const result = await fetchPromise;
-      if (isMounted.current) dispatch({ type: 'SUCCESS', payload: result });
+      const result = await fetchPromise
+      if (isMounted.current) dispatch({ type: 'SUCCESS', payload: result })
     } catch (e) {
-      if (isMounted.current) dispatch({ type: 'ERROR', payload: e });
+      if (isMounted.current) dispatch({ type: 'ERROR', payload: e })
     }
-  } 
+  }
 
-
-  return [state, fetch];
+  return [state, fetch]
 }
-
