01343a10e9e1a9890ec8ceeaabc3d5b0c42a9c11
jorge <sylvanknave@gmail.com>
2020-05-28 06:23:01 -0500

    0.1.0-beta.1 reorganize src files

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,24 +1,2 @@
-import { useEffect, useRef } from 'react'
-
-export function useInterval(callback: () => any, delay: number) {
-  const savedCallback = useRef(() => {
-  })
-
-  // Remember the latest callback.
-  useEffect(() => {
-    savedCallback.current = callback
-  }, [callback])
-
-  // Set up the interval.
-  useEffect(() => {
-    function tick() {
-      savedCallback.current()
-    }
-
-    if (delay !== null) {
-      const id = setInterval(tick, delay)
-      return () => clearInterval(id)
-    }
-    return undefined
-  }, [delay])
-}
+export * from './useFetch'
+export * from './useInterval'
