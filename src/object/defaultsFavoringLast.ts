7f325fa1da71ef224d032a8b7cb3ccd082873c8c
jorge <sylvanknave@gmail.com>
2020-07-09 21:25:55 -0500

    tweak defaultsFavoringLast to filter objects

--- src/object/defaultsFavoringLast.ts
+++ src/object/defaultsFavoringLast.ts
@@ -1,9 +1,10 @@
 /**
  * Merge objects where preference is given to values for a key in subsequent objects
- * @param items a list of objects
+ * @param objects a list of objects
  * @param qualifyValue if this evaluates false for a value, ignore the value
  */
-export const defaultsFavoringLast = (items: object[], qualifyValue = (v: any) => !!v) => {
+export const defaultsFavoringLast = (objects: object[], qualifyValue = (v: any) => !!v) => {
+  const items = objects.filter(x => typeof x === 'object')
   const entries = ([] as [string, any][]).concat(...items.map(Object.entries))
   return entries.reduce(
     (previous, [key, value]) => {
